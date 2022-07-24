6f5b1d9519676b8a884e0c348273ea7c601710c1
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-24 12:01:53 -0600

    Add a working charge method

--- lib/dock.rb
+++ lib/dock.rb
@@ -15,4 +15,21 @@ class Dock
     @customer_rentals.to_h
   end
 
+  def charge(boat)
+  card_to_charge = @customer_rentals.find {|customer_rental| customer_rental[0] == boat}[1].credit_card_number
+  boat_to_charge = @customer_rentals.find {|customer_rental| customer_rental[0] == boat}[0]
+
+  hash = Hash.new(0)
+  hash[:card_number] = card_to_charge
+  # require "pry"
+  # binding.pry
+    if boat_to_charge.hours_rented > @max_rental_time
+      hash[:amount] = @max_rental_time * boat_to_charge.price_per_hour
+      hash
+    elsif boat_to_charge.hours_rented <= max_rental_time
+      hash[:amount] = boat_to_charge.hours_rented * boat_to_charge.price_per_hour
+      hash
+    end
+  end
+
 end
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe do
     expect(dock.max_rental_time).to eq(3)
   end
 
-  it 'has attributes' do
+  it 'can show a log of all rentals as a hash' do
     dock = Dock.new("The Rowing Dock", 3)
     kayak_1 = Boat.new(:kayak, 20)
     kayak_2 = Boat.new(:kayak, 20)
@@ -27,6 +27,33 @@ RSpec.describe do
     hash = { kayak_1 => patrick, kayak_2 => patrick, sup_1 => eugene }
     #is this correct
     expect(dock.rental_log).to eq(hash)
+  end
+
+  it 'can show a hash of a credit card number & amount to charge for a particular boat' do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+    kayak_1.add_hour
+    kayak_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+
+    hash_kayak = { :card_number => "4242424242424242", :amount => 40 }
+
+    expect(dock.charge(kayak_1)).to eq(hash_kayak)
+
+    hash_sup = { :card_number => "1313131313131313", :amount => 45 }
+
+    expect(dock.charge(sup_1)).to eq(hash_sup)
   end
 
 end
