f845833840580467f5af77c963fa0e285762987e
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-24 10:48:53 -0600

    Add a rental_log as a hash

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,9 +1,18 @@
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :customers
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @customer_rentals = []
+  end
+
+  def rent(boat, renter)
+    @customer_rentals << [boat, renter]
+  end
+
+  def rental_log
+    @customer_rentals.to_h
   end
 
 end
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe do
     expect(dock.max_rental_time).to eq(3)
   end
 
+  it 'has attributes' do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+    hash = { kayak_1 => patrick, kayak_2 => patrick, sup_1 => eugene }
+    #is this correct
+    expect(dock.rental_log).to eq(hash)
+  end
+
 end
