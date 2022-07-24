2fd187792fa81c48f8cfb6225d7c03d7da8821f5
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-24 10:30:45 -0600

    Add the add_hours method

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,9 +1,14 @@
 class Boat
-  attr_reader :price_per_hour, :type
+  attr_reader :price_per_hour, :type, :hours_rented
 
   def initialize(type, price_per_hour)
     @type = type
     @price_per_hour = price_per_hour
+    @hours_rented = 0
+  end
+
+  def add_hour
+    @hours_rented += 1
   end
 
 end
--- spec/boat_spec.rb
+++ spec/boat_spec.rb
@@ -11,6 +11,18 @@ RSpec.describe do
     kayak = Boat.new(:kayak, 20)
     expect(kayak.type).to eq(:kayak)
     expect(kayak.price_per_hour).to eq(20)
+    expect(kayak.hours_rented).to eq(0)
+  end
+
+  it 'can be rented for any number of hours' do
+    kayak = Boat.new(:kayak, 20)
+
+    expect(kayak.hours_rented).to eq(0)
+    kayak.add_hour
+    kayak.add_hour
+    kayak.add_hour
+
+    expect(kayak.hours_rented).to eq(3)
   end
 
 
